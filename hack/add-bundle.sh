5d592febefeb209b1d48b23da982a7503ad4db7e
Owen Howard <owen@ziax.com>
2025-05-26 20:20:50 +0100

    Fix OPA and Lokistack gateway references for Cluster Logging

--- hack/add-bundle.sh
+++ hack/add-bundle.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 if [ $# -ne 3 ]; then
-  echo "Usage: $0 <bundle image> <csv> <version>"
+  echo "Usage: $0 <bundle url> <version> <csv>"
   exit 1
 fi
 
 bundle=$1
-csv=$2
-version=$3
+version=$2
+csv=$3
 
-opm render $bundle -o yaml > "catalog/$csv/$csv.v$version.yaml"
+opm render "$bundle:$version" -o yaml > "catalog/$csv/$csv.v$version.yaml"
