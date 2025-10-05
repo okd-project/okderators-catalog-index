12e26fc3369342667d1a3ae4d01c50977f31ce86
Owen Howard <owen@ziax.com>
2025-10-05 17:02:42 +0100

    Fix install script

--- hack/install-catalog.sh
+++ hack/install-catalog.sh
@@ -3,10 +3,7 @@
 NEXT_VERSION="4.20"
 SUPPORTED_VERSIONS="4.15 4.18 4.19"
 
-# Get script directory
-SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-
-CATALOG_YAML=$(cat $SCRIPT_DIR/catalog-source.yaml)
+CATALOG_YAML="$(curl https://raw.githubusercontent.com/okd-project/okderators-catalog-index/refs/heads/release-4.19/hack/catalog-source.yaml)"
 
 # Find installed command oc or kubectl
 if command -v oc &> /dev/null; then
